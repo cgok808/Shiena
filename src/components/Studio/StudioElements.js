4f0c16b054408875aca397557b973042e43a4dff
Chay <chaylin808@gmail.com>
2022-06-15 06:30:45 +0900

    accordion implementation

--- src/components/Studio/StudioElements.js
+++ src/components/Studio/StudioElements.js
@@ -4,3 +4,64 @@ export const StudioSection = styled.section`
   height: 100vh;
   background: linear-gradient(to bottom, #d9a09f 70%, #654a59 30%);
 `;
+
+const AccordionContainer = styled.div`
+  overflow: hidden;
+  width: 18.75rem;
+  border-radius: 0.5rem;
+  background-color: #27262c;
+  color: #f9f9f9;
+`;
+
+const Inner = styled.div`
+  position: absolute;
+  padding: 1rem;
+  color: #c3c1cb;
+`;
+
+const Header = styled.button`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+  height: 4rem;
+  padding: 0 1rem;
+  font-size: 1rem;
+  text-align: left;
+  background: #212025;
+  color: inherit;
+  cursor: pointer;
+`;
+
+const HeaderIcon = styled.span`
+  transform: rotate(${props => (props.isActive ? -180 : 0)});
+  transition: all 0.2s;
+`;
+
+const Content = styled.div`
+  position: relative;
+  overflow: hidden;
+  height: ${props => {
+    const inner = document.getElementById(props.itemName);
+    return `${props.isActive && inner ? inner.clientHeight : 0}px`;
+  }};
+  transition: height 0.35s;
+`;
+
+const AccordionContent = ({ onClick, itemName, itemContent, isActive }) => {
+  return (
+    <>
+      <Header onClick={onClick}>
+        {itemName}
+        <HeaderIcon isActive={isActive}>
+          expand more
+        </HeaderIcon>
+      </Header>
+      <Content itemName={itemName} isActive={isActive}>
+        <Inner id={itemName}>{itemContent}</Inner>
+      </Content>
+    </>
+  );
+};
+
+export { AccordionContainer, AccordionContent };
--- src/components/Studio/index.js
+++ src/components/Studio/index.js
@@ -1,15 +1,69 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import SubHeading from "../SubHeading";
 import { StudioSection } from "./StudioElements";
+import { AccordionContainer, AccordionContent } from "./StudioElements";
+
+const Accordion = ({ items, multiple }) => {
+  const [active, setActive] = useState();
+  const [activeArr, setActiveArr] = useState([]);
+
+  let p = [...items].map(item => {
+    return { name: item.name, active: false };
+  });
+
+  useEffect(() => {
+    setActiveArr(p); // eslint-disable-next-line 
+  }, []);
+
+  const handleClick = name => {
+    setActive(name === active ? null : name);
+    if (multiple) {
+      let ind = activeArr.findIndex(i => i.name === name);
+      let upd = [...activeArr];
+      upd[ind].active = !upd[ind].active;
+      setActiveArr(upd);
+    }
+  };
+
+  return (
+    <AccordionContainer>
+      {items.map(item => {
+        let isActive = active === item.name;
+        if (multiple)
+          isActive = activeArr.some(i => i.name === item.name && i.active);
+        return (
+          <AccordionContent
+            onClick={() => handleClick(item.name)}
+            itemName={item.name}
+            itemContent={item.content}
+            isActive={isActive}
+          />
+        );
+      })}
+    </AccordionContainer>
+  );
+};
 
 const Studio = () => {
+  let items = [
+    {
+      name: "Header 1",
+      content: <div>Lorem ipsum deez</div>,
+    },
+    {
+      name: "Header 2",
+      content: <div>Lorem ipsum deez</div>,
+    },
+    {
+      name: "Header 3",
+      content: <div>Lorem ipsum deez</div>,
+    },
+  ];
+
   return (
-    <StudioSection id="studio">
-      <SubHeading
-        title='studio'
-        subtitle='THE SPECS'
-        color='#513947'
-      />
+    <StudioSection id='studio'>
+      <SubHeading title='studio' subtitle='THE SPECS' color='#513947' />
+        <Accordion multiple items={items} />
     </StudioSection>
   );
 };
